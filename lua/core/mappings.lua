ade95059f96d2de1929e1b28e1a9f512cc8f6794
joeDespres <joe.d@gastrograph.com>
2024-01-21 11:27:36 -0500

    rm og mappings

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -397,20 +397,6 @@ M.gitsigns = {
 
   n = {
     -- Navigation through hunks
-    ["<leader>hn"] = {
-      function()
-        if vim.wo.diff then
-          return "]c"
-        end
-        vim.schedule(function()
-          require("gitsigns").next_hunk()
-        end)
-        return "<Ignore>"
-      end,
-      "Jump to next hunk",
-      opts = { expr = true },
-    },
-
     ["[c"] = {
       function()
         if vim.wo.diff then
@@ -431,64 +417,6 @@ M.gitsigns = {
         require("gitsigns").reset_hunk()
       end,
       "Reset hunk",
-    },
-
-    ["<leader>hd"] = {
-      function()
-        require("gitsigns").toggle_deleted()
-      end,
-      "delete chunk",
-    },
-
-    ["<leader>hs"] = {
-      function()
-        require("gitsigns").stage_hunk()
-      end,
-      "stage buffer",
-    },
-
-    ["<leader>ha"] = {
-      function()
-        require("gitsigns").stage_buffer()
-      end,
-      "stage buffer",
-    },
-
-    ["<leader>hg"] = {
-      function()
-        local user_input = vim.fn.input "Press Enter to run `git push`"
-        local git_cmd = "!git push" -- TODO remember an ssh key
-        print(user_input)
-        if user_input == "" then
-          vim.api.nvim_command(git_cmd)
-        else
-          print "Execution canceled"
-        end
-      end,
-      "push",
-    },
-
-    ["<leader>hm"] = {
-      function()
-        local git_cmd = "!git commit --amend --no-edit"
-        vim.api.nvim_command(git_cmd)
-      end,
-      "git commit",
-    },
-    ["<leader>hc"] = {
-      function()
-        local commit_message = vim.fn.input "Commit message > "
-        local git_cmd = '!git commit -m "' .. commit_message .. '"'
-        vim.api.nvim_command(git_cmd)
-      end,
-      "git commit",
-    },
-
-    ["<leader>hb"] = {
-      function()
-        package.loaded.gitsigns.blame_line()
-      end,
-      "Blame line",
     },
 
     ["<leader>td"] = {
