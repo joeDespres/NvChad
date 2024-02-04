445333ae4941d3afa529b00d91da22d2d824d977
joeDespres <joe.d@gastrograph.com>
2024-02-04 10:26:52 -0500

    auto close nvim tree

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -12,6 +12,29 @@ vim.api.nvim_create_autocmd({ "FocusLost" }, {
   command = "silent! wa",
 })
 
+local function is_modified_buffer_open(buffers)
+  for _, v in pairs(buffers) do
+    if v.name:match "NvimTree_" == nil then
+      return true
+    end
+  end
+  return false
+end
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  nested = true,
+  callback = function()
+    if
+      #vim.api.nvim_list_wins() == 1
+      and vim.api.nvim_buf_get_name(0):match "NvimTree_" ~= nil
+      and is_modified_buffer_open(vim.fn.getbufinfo { bufmodified = 1 })
+        == false
+    then
+      vim.cmd "quit"
+    end
+  end,
+})
+
 vim.api.nvim_create_augroup("AutoFormat", {})
 
 vim.api.nvim_create_autocmd("BufWritePost", {
