4702cfaa2cddbccf0bec135533e9046860ef4a9f
joeDespres <joe.d@gastrograph.com>
2024-03-02 10:26:13 -0500

    change git colors

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,5 +1,12 @@
 local M = {}
 M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
-M.ui = { theme = "catppuccin" }
+M.ui = {
+  theme = "catppuccin",
+  hl_override = {
+    DiffChange = { fg = "orange" },
+    DiffAdded = { fg = "purple" },
+    DiffRemoved = { fg = "red" },
+  },
+}
 return M
